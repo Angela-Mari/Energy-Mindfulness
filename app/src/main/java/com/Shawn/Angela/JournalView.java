7e0b27c24eb10d725e524a0fd4f621f5465839d1
Angela George <ageorge5@zagmail.gonzaga.edu>
2021-12-10 14:09:16 -0800

    xml files

---
+++ app/src/main/java/com/Shawn/Angela/JournalView.java
@@ -0,0 +1,14 @@
+package com.Shawn.Angela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JournalView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journal_view);
+    }
+}
