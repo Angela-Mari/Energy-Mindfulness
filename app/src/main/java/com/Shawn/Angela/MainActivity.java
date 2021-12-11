f25077c27ed3ecd0688004e45d86acf19327dd5f
Angela George <ageorge5@zagmail.gonzaga.edu>
2021-12-10 17:10:05 -0800

    Create journal and pass back to mainActivty

--- app/src/main/java/com/Shawn/Angela/MainActivity.java
+++ app/src/main/java/com/Shawn/Angela/MainActivity.java
@@ -1,10 +1,15 @@
 package com.Shawn.Angela;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Notification;
 import android.content.DialogInterface;
@@ -12,22 +17,30 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String TAG = "MainActivtyTAG";
     private NotificationManagerCompat notificationManager;
-
+    Button createJournalEntry;
     TextView batteryText;
+
+    ActivityResultLauncher<Intent> launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         notificationManager = NotificationManagerCompat.from(this);
 
+        // get battery percentage
         setContentView(R.layout.activity_main);
         batteryText = findViewById(R.id.batteryLife);
 
@@ -42,8 +55,46 @@ public class MainActivity extends AppCompatActivity {
 
         batteryText.setText("Current Battery Percentage: " + String.valueOf(batteryPct));
 
-        //sendNotification(batteryPct);
+        launcher = launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Log.d(TAG, "onActivityResult: ");
+                        // this callback executes when MainActivity returns from
+                        // starting an activity (e.g. SecondActivity) that was
+                        // started for a result
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Log.d(TAG, "activity result OK");
+                            Intent data = result.getData();
+                            String title = data.getStringExtra("title");
+                            String date = data.getStringExtra("date");
+                            String time = data.getStringExtra("time");
+                            String mood = data.getStringExtra("mood");
+                            String journalEntry = data.getStringExtra("journalEntry");
+                            double battery = data.getDoubleExtra("battery", -1);
 
+                            Journal newJournal = new Journal(title, date, time, journalEntry, mood, battery);
+                            Log.d(TAG, "onActivityResult: New Journal Entry: " + newJournal.toString());
+                        }
+                        else {
+                            Log.d(TAG, "onActivityResult: activity result not ok");
+                        }
+
+                    }
+                });
+
+        // launch CreateJournalActivity
+        createJournalEntry = findViewById(R.id.createJournalButton);
+        createJournalEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: launch create entry");
+                Intent intent = new Intent(MainActivity.this, CreateJournalActivity.class);
+                launcher.launch(intent);
+            }
+        });
+
+        // calendar view
         CalendarView calendarView = findViewById(R.id.calendarView);
         if (calendarView != null) {
             calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
